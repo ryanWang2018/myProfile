636294459f3acb48a4d1d6d41aba6f71fba93666
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-04-08 22:05:06 -0400

    add landing svg animation

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,44 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Credits from "./components/credits";
 import Food from "./components/food.jsx";
 import UploadForm from "./components/uploadForm.jsx";
+import ReactSVG from "react-svg";
+import "./index.css";
+import svg from "./components/svgText.svg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countDown: 1
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ countDown: this.state.countDown - 1 });
+    }, 3000);
+  }
+
   render() {
-    return (
-      // <Food
-      //   img="http://infinityflamesoft.com/html/restarunt-preview/assets/img/menu/menu-2.jpg"
-      //   name="Chicken Fried Salad"
-      //   price="$45"
-      //   info="Aperiam tempore sit, perferendis numquam repudiandae porro voluptate dicta seape facilis."
-      // />
-      <UploadForm />
-    );
+    if (this.state.countDown === 1) {
+      return (
+        <div className="wrapper">
+          <ReactSVG src={svg} svgClassName="./index.css" />
+        </div>
+      );
+    } else {
+      return (
+        // <Food
+        //   img="http://infinityflamesoft.com/html/restarunt-preview/assets/img/menu/menu-2.jpg"
+        //   name="Chicken Fried Salad"
+        //   price="$45"
+        //   info="Aperiam tempore sit, perferendis numquam repudiandae porro voluptate dicta seape facilis."
+        // />
+        <div>
+          <UploadForm />
+        </div>
+      );
+    }
   }
 }
 
---
+++ client/src/components/landingPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  state = {};
+  render() {
+    return <h1>H</h1>;
+  }
+}
+
+export default LandingPage;
